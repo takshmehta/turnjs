286a0dfda326d772d5c1df66a6d0dff1599cf5d2
Ram kumar <rk16013093@gmail.com>
2020-11-23 16:09:34 +0530

    making code readable

--- index.js
+++ index.js
@@ -1,66 +1,75 @@
-var s=true;
+// on turn properties of flipbook div change using turnjs which are mention in function as:
 $("#flipbook").turn({
     width: 1020,
     height: 790,
-    duration:3200
+    duration:3200 //duration of page flip
 });
 
 // auto turn bottom right corner using peel effect of turnjs 
 $("#flipbook").turn("peel", "br");
 
 
+// developer write code when an either right or left button click is clicked 
+// code orders matters in both of these function
+$('.right-icon-div').click(function(){
+  $('.right-icon-div').css({'right':'10rem'})
+  $('.left-icon-div').css('display','block')
+  $('#flipbook').turn('next');
+  jQuery(document).ready(inBetweenPage);
+  jQuery(document).ready(lastPage);    
+})
+
+$('.left-icon-div').click(function(){
+$('.right-icon-div').css({'display':'block'})
+$('#flipbook').turn('previous');
+jQuery(document).ready(lastPagePaddingReduce);
+jQuery(document).ready(firstPage);
+})
+
+
+
+// devloper write code  when keydown either left arrow or right arrow 
 $(window).bind('keydown', function(e){
-    if (e.keyCode==37){
-      $('#flipbook').turn('previous');
+    if (e.keyCode==37)
+    { 
+      $('#flipbook').turn('previous'); //turnjs  method 
+      // developer write code when p1 class ( p1 means page1 )is visible 
       if($('.p1').is(':visible')){
-      document.querySelector('.m1').style.paddingLeft='0rem';
+          document.querySelector('.flipbookContainer').style.paddingLeft='0rem';
       }
-    
-} else if (e.keyCode==39)
-    
-  
-    // $('.right-icon-div').css({'right':'10rem'})
-    // $('.left-icon-div').css('display','block')
-  
-    $('#flipbook').turn('next');
-    document.querySelector('.m1').style.paddingLeft='20rem';
-    document.querySelector('.left-icon-div').style.display='block';
-    document.querySelector('.right-icon-div').style.right='10rem';
-    if($('.p14').is(':visible')){
-      document.querySelector('.m1').style.paddingLeft='40rem';
-      document.querySelector('.left-icon-div').style.display='none';
-      document.querySelector('.right-icon-div').style.right='22rem';
-    }
-      //  jQuery(document).ready(inBetweenPage);
-      //  jQuery(document).ready(lastPage);
-       
-
-    }
-    );
+    }else if (e.keyCode==39)
+      {
+          $('#flipbook').turn('next');   //turnjs  method 
+          // developer write code 
+          document.querySelector('.flipbookContainer').style.paddingLeft='20rem';
+          document.querySelector('.left-icon-div').style.display='block';
+          document.querySelector('.right-icon-div').style.right='10rem';
+          // developer write code when p14 class ( p14 means page14 )is visible 
+          if($('.p14').is(':visible'))
+          {
+               document.querySelector('.flipbookContainer').style.paddingLeft='40rem';
+               document.querySelector('.left-icon-div').style.display='none';
+               document.querySelector('.right-icon-div').style.right='22rem';
+          }
+      }
+  });
 
 
-
-
-
-
-
-   // jQuery(document).ready(lastPagePaddingReduce);
-
+// developer write code when inbetween page is visible (inbetween means page2 to last page )
 function inBetweenPage () {
   if($('.p2').is(':visible')){ //if the container is visible on the page
-    document.querySelector('.m1').style.paddingLeft='20rem';
+    document.querySelector('.flipbookContainer').style.paddingLeft='20rem';
     document.querySelector('body').style.overflow='hidden';
   } else  {
-    setTimeout(inBetweenPage, 50); //wait 50 ms, then try again
-   
+    setTimeout(inBetweenPage, 50); //wait 50 ms, then try again 
   }
 }
 
  
+// developer write code when last page is visible 
 function lastPage () {
-  if($('.p14').is(':visible')){ //if the container is visible on the page
-    //alert('ready')  //Adds a grid to the html
-    document.querySelector('.m1').style.paddingLeft='40rem';
+  if($('.p14').is(':visible')){ //if the page14 is visible on the page
+    document.querySelector('.flipbookContainer').style.paddingLeft='40rem';
     document.querySelector('body').style.overflow='hidden';
     $('.right-icon-div').css({'display':'none'})
     $('.left-icon-div').css('left','38rem')
@@ -69,10 +78,11 @@ function lastPage () {
   }
 }
 
+// developer write code 
 function lastPageForLeftButton(){
   if($('.p14').is(':visible')){ //if the container is visible on the page
-    // alert('12')
-    document.querySelector('.m1').style.paddingLeft='0rem';
+    
+    document.querySelector('.flipbookContainer').style.paddingLeft='0rem';
     document.querySelector('body').style.overflow='hidden';
     $('.right-icon-div').css({'display':'block'})
     $('.left-icon-div').css('left','18rem')
@@ -84,10 +94,10 @@ function lastPageForLeftButton(){
 
 }
 
+// developer write code 
 function lastPagePaddingReduce () {
   if($('.p13.odd').is(':visible')){ //if the container is visible on the page
-    // alert('12')
-    document.querySelector('.m1').style.paddingLeft='20rem';
+    document.querySelector('.flipbookContainer').style.paddingLeft='20rem';
     document.querySelector('body').style.overflow='hidden';
     $('.right-icon-div').css({'display':'block'})
     $('.left-icon-div').css('left','18rem')
@@ -98,11 +108,11 @@ function lastPagePaddingReduce () {
   }
 }
 
-
+//developer write code
 function firstPage () {
     if($('.p1').is(':visible')){ //if the container is visible on the page
      
-      document.querySelector('.m1').style.paddingLeft='0rem';
+      document.querySelector('.flipbookContainer').style.paddingLeft='0rem';
       document.querySelector('body').style.overflow='hidden';
       $('.right-icon-div').css({'right':'30rem'})
       $('.left-icon-div').css('display','none')
@@ -112,6 +122,10 @@ function firstPage () {
     }
   }
 
+
+var s=true; //for toggle button of carousel
+
+// developer write code for toggle button on botton right corner of magazine
 $('.carousel-toggle-div').click(function(){
   if(s){
     $('#demo').css({"display":"block"});
@@ -136,41 +150,23 @@ $('.carousel-toggle-div').click(function(){
 })
 
 
-// icon click events
-
-$('.right-icon-div').click(function(){
-  $('.right-icon-div').css({'right':'10rem'})
-  $('.left-icon-div').css('display','block')
-  console.log('right icon')
-  $('#flipbook').turn('next');
-     jQuery(document).ready(inBetweenPage);
-     jQuery(document).ready(lastPage);
-     
-})
-
-
-$('.left-icon-div').click(function(){
-  console.log('left icon')
-  $('.right-icon-div').css({'display':'block'})
- 
-  $('#flipbook').turn('previous');
-  jQuery(document).ready(lastPagePaddingReduce);
-  jQuery(document).ready(firstPage);
-
-
-})
-
-function fuck(){
-  $('.right-icon-div').css({'display':'block'})
- 
-  $('#flipbook').turn('previous');
-  jQuery(document).ready(lastPagePaddingReduce);
-  jQuery(document).ready(firstPage);
-
-}
-
+// adding a shadow on our magazine its not possible to assign directly using class that's why use this type 
 const firstPageShadow=document.querySelector('.p1');
-
 const newElement=firstPageShadow.parentElement.parentElement;
-
 newElement.classList.add('first-page-shadow');
+
+
+// ------------------------------  IN TESTING ----------------------------------------------------------------
+// image store 
+var imageStore = '{ "imagesHouse" : [' +
+'{ "img":"https://beeimg.com/images/i26660926801.png" , "id":"1" },' +
+'{ "img":"https://beeimg.com/images/d12223208083.png" , "id":"2" },' +
+'{ "img":"https://beeimg.com/images/f65081686483.png" , "id":"3" } ]}';
+
+var obj = JSON.parse(imageStore);
+
+for(i=2;i<5;i++){
+  let select="."+"p"+i+" "+"img";
+  document.querySelector(select).setAttribute("src",obj.imagesHouse[i-1].img)
+  console.log(document.querySelector(select));
+}
